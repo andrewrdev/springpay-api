4c60380942dbd6423ee6182e796bf3ac54d02851
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2024-01-15 17:53:48 -0300

    feat: TransactionController

---
+++ src/main/java/andrewrdev/SpringPay/controllers/TransactionController.java
@@ -0,0 +1,10 @@
+package andrewrdev.SpringPay.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionController {
+    
+}
