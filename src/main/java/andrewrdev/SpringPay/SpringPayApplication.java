b06bda7bd902b024c49716e256c6f9cb365ff68c
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2024-01-29 13:44:15 -0300

    project update

---
+++ src/main/java/andrewrdev/SpringPay/SpringPayApplication.java
@@ -0,0 +1,13 @@
+package andrewrdev.SpringPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPayApplication.class, args);		
+	}
+
+}
